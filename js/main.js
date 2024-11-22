4835ca17f344acd5dc54ca0b728f0ace9b4fa8d4
HuckleberryTerry <jozef.jenkins19@gmail.com>
2024-11-22 11:08:03 +0000

    Updated main.js
    I have added in comments for different sections and have added an on('click') jQuery event to handle nav bar selection.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//------------------VARIABLES---------------------//
+//text effect variables
 const $heroTitle = $('#myName');
 const $heroText = $('#myDescription');
 const hero_Title_str = 'Hello, I am Jozef Jenkins';
@@ -5,6 +7,22 @@ const hero_Text_str = "I'm a web developer"
 const speed = 100;
 let i = 0;
 
+//Hamburger Menu Variables
+const $isActive = $('.isActive');
+
+
+//Functions
+/** Type-Writer Effect using arrays
+* Splits a string into an array and appends each character to a specified class or id.
+*
+* @param {jQuery selector} name - the class or id of an element
+*
+* @param {string} text - The string that you want to appear in the element
+*
+* @example
+*
+* splitStr($(#myName), 'Jozef Jenkins'); //appends the string to the specified class or id, letter by letter.
+*/
 const splitStr = (name, text)=>{
     let hero_arr = text.split('');
     setTimeout(function() {
@@ -13,6 +31,7 @@ const splitStr = (name, text)=>{
             name.append(hero_arr[i]);
             i++;
         }
+        return name;
     }, speed); 
 }
 
@@ -26,9 +45,15 @@ const splitStr = (name, text)=>{
 //     }
 //   }
 
+
 jQuery(function() {
     splitStr($heroTitle, hero_Title_str);
-    // splitStr($heroText, hero_Text_str);
+    //splitStr($heroText, hero_Text_str);
     $heroText.hide();
-    $heroText.fadeIn(3000);
+    $heroText.fadeIn(4000);
+})
+
+$isActive.on('click', function(event){
+    $isActive.children().addClass('__active');
+    $('.__active').children('ul li a').addClass('__dropped');
 })
